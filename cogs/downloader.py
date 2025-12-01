cb7044b385b8fca8a0a3f00237f5919fbd8dccda
ThakinVunc <57339123+ThakinVunc@users.noreply.github.com>
2025-12-01 17:00:00 -0500

    Simplified download code and added error handling

--- cogs/downloader.py
+++ cogs/downloader.py
@@ -22,69 +22,70 @@ class Downloader(commands.Cog):
         self.bot = bot
     
     def download_media(self, url, formattype):
-        if formattype == 'audio':
+        try:
             randomString = ''.join(random.choice(string.ascii_letters) for x in range(10))
-            ydl_opts = {
-                'outtmpl': f'{mp3Dir}//%(title)s ({randomString}).%(ext)s',
-                'format': 'mp3/bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                }]
-            }
+            if formattype == 'mp3':
+                ydl_opts = {
+                    'outtmpl': f'{mp3Dir}//%(title)s ({randomString}).%(ext)s',
+                    'format': 'mp3/bestaudio/best',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                    }]
+                }
+                saveDir = mp3Dir
+            elif formattype == 'mp4':
+                ydl_opts = {
+                    'outtmpl': f'{mp4Dir}//%(title)s ({randomString}).%(ext)s',
+                    'format': 'mp4'
+                }
+                saveDir = mp4Dir
+
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 infoDict = ydl.extract_info(url, download=False)
                 rawTitle = infoDict.get('title', None)
                 error_code = ydl.download(url)
+
             title = rawTitle.translate(str.maketrans('','', string.punctuation)) + f"_({randomString})"
-            os.rename(f'{mp3Dir}//{rawTitle} ({randomString}).mp3', f'{mp3Dir}//{title}.mp3')
-            size = os.path.getsize(f".//cogs//resources//mp3//{title}.mp3")
+            os.rename(f'{saveDir}//{rawTitle} ({randomString}).{formattype}', f'{saveDir}//{title}.{formattype}')
+            size = os.path.getsize(f"{saveDir}//{title}.{formattype}")
             varlist = [title, rawTitle]
             if size > 10000000:
-                with open(f'{mp3Dir}//{title}.mp3', 'rb') as mp3:
-                    url = requests.post('https://uguu.se/upload.php', files={'files[]': mp3}).json()['files'][0]['url']
+                with open(f'{saveDir}//{title}.{formattype}', 'rb') as file:
+                    url = requests.post('https://uguu.se/upload.php', files={'file': file}).json()['files'][0]['url']
                     return {"title":title, "rawTitle":rawTitle,"url":url}
             elif size < 10000000:
                 return {"title":title, "rawTitle":rawTitle}
-        elif formattype == 'video':
-            randomString = ''.join(random.choice(string.ascii_letters) for x in range(10))
-            ydl_opts = {
-                'outtmpl': f'{mp4Dir}//%(title)s ({randomString}).%(ext)s',
-                'format': 'mp4'
-            }
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                infoDict = ydl.extract_info(url, download=False)
-                rawTitle = infoDict.get('title', None)
-                error_code = ydl.download(url)
-            title = rawTitle.translate(str.maketrans('','', string.punctuation)) + f"_({randomString})"
-            os.rename(f'{mp4Dir}//{rawTitle} ({randomString}).mp4', f'{mp4Dir}//{title}.mp4')
-            size = os.path.getsize(f".//cogs//resources//mp4//{title}.mp4")
-            if size > 10000000:
-                with open(f'{mp4Dir}//{title}.mp4', 'rb') as mp4:
-                    url = requests.post('https://uguu.se/upload.php', files={'files[]': mp4}).json()['files'][0]['url']
-                    return {"title":title, "rawTitle":rawTitle,"url":url}
-            elif size < 10000000:
-                return {"title":title, "rawTitle":rawTitle}
-    
+        except yt_dlp.utils.DownloadError as e:
+            return {"error":e}
+
     @discord.app_commands.command()
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-    async def download(self, interaction: discord.Interaction, url: str, formattype: Literal['video','audio']):
+    @app_commands.choices(formatchoice=[
+        app_commands.Choice(name='video', value="mp4"),
+        app_commands.Choice(name='audio', value="mp3"),
+    ])
+    async def download(self, interaction: discord.Interaction, url: str, formatchoice: app_commands.Choice[str]) -> None:
+        formattype = formatchoice.value
+        if formattype == 'mp3':
+            saveDir = mp3Dir
+        elif formattype == 'mp4':
+            saveDir = mp4Dir
+        
         if validators.url(url) == True:
             message = await interaction.response.send_message(f"Downloading '{url}', please wait.", ephemeral=True)
-            print(formattype)
             loop = asyncio.get_event_loop()
             downloadData = await loop.run_in_executor(ThreadPoolExecutor(), functools.partial(self.download_media, url, formattype))
-            
-            if downloadData.get('url') is not None:
-                await interaction.followup.send(f"Downloaded [{downloadData["rawTitle"]}](<{downloadData["url"]}>) \n Link: {downloadData["url"]}")
+            if downloadData.get('error') is None:
+                if downloadData.get('url') is not None:
+                    await interaction.followup.send(f"Downloaded [{downloadData["rawTitle"]}](<{url}>)\nLink: {downloadData["url"]}")
+                else:
+                    await interaction.followup.send(f"Downloaded [{downloadData["rawTitle"]}](<{url}>)", file = discord.File(f'{saveDir}//{downloadData["title"]}.{formattype}'))
+
+                os.remove(f'{saveDir}//{downloadData["title"]}.{formattype}')
             else:
-                await interaction.followup.send(f"Downloaded [{downloadData["rawTitle"]}](<{url}>)", file = discord.File(f'{mp3Dir}//{downloadData["title"]}.mp3'))
-                
-            if formattype == 'audio':
-                os.remove(f'{mp3Dir}//{downloadData["title"]}.mp3')
-            elif formattype == 'video':
-                os.remove(f'{mp4Dir}//{downloadData["title"]}.mp4')
+                await interaction.followup.send(f'Download failed\nError: ``{downloadData["error"]}``')
         else:
             await interaction.response.send_message(f"'{url}' is not a valid url.", ephemeral=True)
 
