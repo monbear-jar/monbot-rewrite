262ad211e2eb8e5932dfb60d8886ceab3836764c
ThakinVunc <57339123+ThakinVunc@users.noreply.github.com>
2025-12-03 09:23:33 -0500

    New cog for bot management commands

---
+++ cogs/botmanagement.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+import os, sys
+import json
+
+botInfo = ".//bot_info.json"
+with open(botInfo, 'r') as f:
+    data = json.loads(f.read())
+    ownerID = data["ownerID"]
+
+class BotManagement(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def is_me():
+        def predicate(interaction: discord.Interaction) -> bool:
+            return interaction.user.id == int(ownerID)
+        return app_commands.check(predicate)
+
+    def restart_bot(self):
+        os.execv(sys.executable, ['python'] + sys.argv)
+
+    @discord.app_commands.command()
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @is_me()
+    async def restart(self, interaction: discord.Interaction):
+        await interaction.response.send_message('Restarting bot...')
+        self.restart_bot()
+
+async def setup(bot):
+    await bot.add_cog(BotManagement(bot))
+
+async def teardown(bot):
+    await bot.remove_cog('BotManagement')
